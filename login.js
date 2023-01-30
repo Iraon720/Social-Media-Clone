425eec3b60e73fac9c7f31595882e57ffc4528ec
Iraon720 <iraon3493@gmail.com>
2023-01-30 10:28:54 -0500

    debugging login

--- login.js
+++ login.js
@@ -5,86 +5,110 @@ const user = {
   email: 'iraon720@gmail.com',
   username: 'iraon720',
 };
-const retrieveUser = (param) => fetch(`${User_URL}/${param.username}${EXT}`);
+// const retrieveUser = (param) => fetch(`${User_URL}/${param.username}${EXT}`);
 
 //Login Mobile
-const username = document.getElementById('Email');
-const password = document.getElementById('password');
-const loginBtnD = document.getElementById('login-btnD');
+// const username = document.getElementById('Email');
+// const password = document.getElementById('password');
+// const loginBtnD = document.getElementById('login-btnD');
 
-username.addEventListener('keyup', (e) => {
-  console.log(username.value);
-});
+// username.addEventListener('keyup', (e) => {
+//   console.log(username.value);
+// });
 
-password.addEventListener('keyup', (e) => {
-  console.log(password.value);
-});
+// password.addEventListener('keyup', (e) => {
+//   console.log(password.value);
+// });
 
 //Validation
-function browserValidation() {
-  if (!password.value || !password.value.length > 8) {
-    return 'Invalid password';
-  }
-  return null;
-}
-loginBtnD.addEventListener('click', handleLogin);
+// function browserValidation() {
+//   if (!password.value || !password.value.length > 8) {
+//     return 'Invalid password';
+//   }
+//   return null;
+// }
+// loginBtnD.addEventListener('click', handleLogin);
 
-function handleLogin(e) {
-  e.preventDefault();
-  e.stopPropagation();
+// function handleLogin(e) {
+//   e.preventDefault();
+//   e.stopPropagation();
 
-  window.location.href = 'dashboard.html';
+//   window.location.href = 'dashboard.html';
+// }
+const loginBtn = document.getElementById('login-btnD');
+loginBtn.addEventListener('click',(e) => {
+  event.preventDefault();
+  const username = document.getElementById('Email').value;
+  const password = document.getElementById('password');
 }
+fetch (User_URL)
+.then((response) => response.json())
+.then((data) => {
+  const expressU = Object.value(data);
+  const expressUs = expressU.find (
+    (user) => user.email === email && user.password ===
+  )
+}
+if (expressUs) {
+  console.log('Welcome Back!');
+  window.location.href ='dashboard.html';
+} else {
+  console.log('Invalid username or password');
+}
+})
+.catch((error) => console.error(error));
+} );
+
 ////////////
 
 //Login Desktop
-const usernameDsk = document.getElementById('Email-desktop');
-const passwordDsk = document.getElementById('password-desktop');
+// const usernameDsk = document.getElementById('Email-desktop');
+// const passwordDsk = document.getElementById('password-desktop');
 
-usernameDsk.addEventListener('keyup', (e) => {
-  console.log(usernameDsk.value);
-});
+// usernameDsk.addEventListener('keyup', (e) => {
+//   console.log(usernameDsk.value);
+// });
 
-passwordDsk.addEventListener('keyup', (e) => {
-  console.log(passwordDsk.value);
-});
+// passwordDsk.addEventListener('keyup', (e) => {
+//   console.log(passwordDsk.value);
+// });
 
-//Validation
-function browserValidation() {
-  if (!passwordDsk.value || !passwordDsk.value.length > 8) {
-    return 'Invalid password';
-  }
-  return null;
-}
-//Try & Catch
-loginBtnD.addEventListener('click', handleLogin);
-const expressUser = {
-  username: username.value,
-  password: password.value,
-};
-async function handleLogin(e) {
-  e.preventDefault();
-  e.stopPropagation();
+// //Validation
+// function browserValidation() {
+//   if (!passwordDsk.value || !passwordDsk.value.length > 8) {
+//     return 'Invalid password';
+//   }
+//   return null;
+// }
+// //Try & Catch
+// loginBtnD.addEventListener('click', handleLogin);
+// const expressUser = {
+//   username: username.value,
+//   password: password.value,
+// };
+// async function handleLogin(e) {
+//   e.preventDefault();
+//   e.stopPropagation();
 
-  try {
-    const validateError = browserValidation();
-    if (validateError) {
-      throw Error(validateError);
-    }
+//   try {
+//     const validateError = browserValidation();
+//     if (validateError) {
+//       throw Error(validateError);
+//     }
 
-    const userExists = await retrieveUser(expressUser);
-    if (!userExists.ok) {
-      throw Error('Error retrieving user');
-    }
-    const databaseExistence = await userExists.json();
-    if (!databaseExistence) {
-      throw Error('User does not exist');
-    }
-    if (databaseExistence) {
-      localStorage.setItem('userInfo', JSON.stringify(databaseExistence));
-      location.replace('/dashboard.html');
-    }
-  } catch (error) {
-    alert(error);
-  }
-}
+//     const userExists = await retrieveUser(expressUser);
+//     if (!userExists.ok) {
+//       throw Error('Error retrieving user');
+//     }
+//     const databaseExistence = await userExists.json();
+//     if (!databaseExistence) {
+//       throw Error('User does not exist');
+//     }
+//     if (databaseExistence) {
+//       localStorage.setItem('userInfo', JSON.stringify(databaseExistence));
+//       location.replace('/dashboard.html');
+//     }
+//   } catch (error) {
+//     alert(error);
+//   }
+// }
