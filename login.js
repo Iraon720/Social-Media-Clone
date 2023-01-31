c0d013668b397e1f386f8b295690efea2a9351c2
Iraon720 <iraon3493@gmail.com>
2023-01-31 00:00:52 -0500

    final commit

--- login.js
+++ login.js
@@ -4,111 +4,154 @@ const EXT = '.json';
 const user = {
   email: 'iraon720@gmail.com',
   username: 'iraon720',
+  password: 'Creatine89!',
 };
-// const retrieveUser = (param) => fetch(`${User_URL}/${param.username}${EXT}`);
+const retrieveUser = (param) => fetch(`${User_URL}/${param.username}${EXT}`);
 
 //Login Mobile
-// const username = document.getElementById('Email');
-// const password = document.getElementById('password');
-// const loginBtnD = document.getElementById('login-btnD');
+const username = document.getElementById('Email');
+const password = document.getElementById('password');
+const loginBtnD = document.getElementById('login-btnD');
 
-// username.addEventListener('keyup', (e) => {
-//   console.log(username.value);
-// });
+username.addEventListener('keyup', (e) => {
+  console.log(username.value);
+});
 
-// password.addEventListener('keyup', (e) => {
-//   console.log(password.value);
-// });
+password.addEventListener('keyup', (e) => {
+  console.log(password.value);
+});
 
 //Validation
-// function browserValidation() {
-//   if (!password.value || !password.value.length > 8) {
-//     return 'Invalid password';
-//   }
-//   return null;
-// }
-// loginBtnD.addEventListener('click', handleLogin);
-
-// function handleLogin(e) {
-//   e.preventDefault();
-//   e.stopPropagation();
-
-//   window.location.href = 'dashboard.html';
-// }
-const loginBtn = document.getElementById('login-btnD');
-loginBtn.addEventListener('click',(e) => {
-  event.preventDefault();
-  const username = document.getElementById('Email').value;
-  const password = document.getElementById('password');
+function browserValidation() {
+  if (!password.value || !password.value.length > 8) {
+    return 'Invalid password';
+  }
+  return null;
 }
-fetch (User_URL)
-.then((response) => response.json())
-.then((data) => {
-  const expressU = Object.value(data);
-  const expressUs = expressU.find (
-    (user) => user.email === email && user.password ===
-  )
-}
-if (expressUs) {
-  console.log('Welcome Back!');
-  window.location.href ='dashboard.html';
-} else {
-  console.log('Invalid username or password');
-}
-})
-.catch((error) => console.error(error));
-} );
+loginBtnD.addEventListener('click', handleLogin);
 
-////////////
+function handleLogin(e) {
+  e.preventDefault();
+  e.stopPropagation();
+
+  window.location.href = 'dashboard.html';
+}
 
 //Login Desktop
-// const usernameDsk = document.getElementById('Email-desktop');
-// const passwordDsk = document.getElementById('password-desktop');
+const usernameDsk = document.getElementById('Email-desktop');
+const passwordDsk = document.getElementById('password-desktop');
 
-// usernameDsk.addEventListener('keyup', (e) => {
-//   console.log(usernameDsk.value);
-// });
+usernameDsk.addEventListener('keyup', (e) => {
+  console.log(usernameDsk.value);
+});
 
-// passwordDsk.addEventListener('keyup', (e) => {
-//   console.log(passwordDsk.value);
-// });
+passwordDsk.addEventListener('keyup', (e) => {
+  console.log(passwordDsk.value);
+});
 
-// //Validation
-// function browserValidation() {
-//   if (!passwordDsk.value || !passwordDsk.value.length > 8) {
-//     return 'Invalid password';
-//   }
-//   return null;
-// }
-// //Try & Catch
-// loginBtnD.addEventListener('click', handleLogin);
-// const expressUser = {
-//   username: username.value,
-//   password: password.value,
+//Validation
+function browserValidation() {
+  if (!passwordDsk.value || !passwordDsk.value.length > 8) {
+    return 'Invalid password';
+  }
+  return null;
+}
+//Try & Catch
+loginBtnD.addEventListener('click', handleLogin);
+const expressUser = {
+  username: username.value,
+  password: password.value,
+};
+async function handleLogin(e) {
+  e.preventDefault();
+  e.stopPropagation();
+
+  try {
+    const validateError = browserValidation();
+    if (validateError) {
+      throw Error(validateError);
+    }
+
+    const userExists = await retrieveUser(expressUser);
+    if (!userExists.ok) {
+      throw Error('Error retrieving user');
+    }
+    const databaseExistence = await userExists.json();
+    if (!databaseExistence) {
+      throw Error('User does not exist');
+    }
+    if (databaseExistence) {
+      localStorage.setItem('userInfo', JSON.stringify(databaseExistence));
+      location.replace('/dashboard.html');
+    }
+  } catch (error) {
+    alert(error);
+  }
+}
+
+/////////////////////does not work properly////////
+// const config = {
+//   email: 'iraon720@gmail.com',
+//   username: 'iraon720',
+//   password: 'Creatine89!',
 // };
-// async function handleLogin(e) {
+// firebase.initializeApp(config);
+
+// // Get references to the form elements
+// const emailInput = document.getElementById('Email');
+// const passwordInput = document.getElementById('password');
+// const submitButton = document.getElementById('login-btnD');
+
+// // Add a click event listener to the submit button
+// submitButton.addEventListener('click', function (event) {
+//   event.preventDefault();
+
+//   // Get the email and password values from the form
+//   var email = emailInput.value;
+//   var password = passwordInput.value;
+
+//   // Sign in the user with email and password
+//   firebase
+//     .auth()
+//     .signInWithEmailAndPassword(email, password)
+//     .then(function (user) {
+//       // User successfully signed in
+//       console.log('Successful sign in with user:', user);
+
+//       // Redirect the user to the home page or show a success message
+//       window.location.href = 'dashboard.html';
+//     })
+//     .catch(function (error) {
+//       // An error occurred while signing in the user
+//       console.error('Error signing in user:', error);
+
+//       // Show an error message
+//       alert(
+//         'The email and password you entered do not match our records. Please try again.'
+//       );
+//     });
+// });
+
+// const loginBtn = document.getElementById('login-btnD');
+// loginBtn.addEventListener('click', (e) => {
 //   e.preventDefault();
-//   e.stopPropagation();
+//   const email = document.getElementById('Email').value;
+//   const password = document.getElementById('password').value;
 
-//   try {
-//     const validateError = browserValidation();
-//     if (validateError) {
-//       throw Error(validateError);
-//     }
+//   fetch(User_URL)
+//     .then((response) => response.json())
+//     .then((data) => {
+//       const expressU = Object.value(data);
+//       const expressUs = expressU.find(
+//         (user) => user.email === email && user.password === password
+//       );
 
-//     const userExists = await retrieveUser(expressUser);
-//     if (!userExists.ok) {
-//       throw Error('Error retrieving user');
-//     }
-//     const databaseExistence = await userExists.json();
-//     if (!databaseExistence) {
-//       throw Error('User does not exist');
-//     }
-//     if (databaseExistence) {
-//       localStorage.setItem('userInfo', JSON.stringify(databaseExistence));
-//       location.replace('/dashboard.html');
-//     }
-//   } catch (error) {
-//     alert(error);
-//   }
-// }
+//       if (expressUs) {
+//         console.log('Welcome Back!');
+//         window.location.href = 'dashboard.html';
+//       } else {
+//         console.log('Invalid username or password');
+//       }
+//     })
+//     .catch((error) => console.error(error));
+// });
--- newsfeed.js
+++ newsfeed.js
@@ -4,10 +4,8 @@ const Post_URL =
   'https://twitter-clone-44098-default-rtdb.firebaseio.com/posts';
 const EXT = '.json';
 
-let postText = document.getElementById('textarea');
 let postBtn = document.getElementById('post-btn');
-let editBtn = document.getElementById('edit-btn');
-let deleteBtn = document.getElementById('delete-btn');
+let postText = newPost.querySelector('textarea');
 const appendDiv = document.getElementById('c1d');
 
 postBtn.addEventListener('click', (e) => {
@@ -29,6 +27,80 @@ postBtn.addEventListener('click', (e) => {
         `;
   appendDiv.prepend(newPost);
 });
-function deletePost(
-  
-)
+//Delete post
+let deleteBtn = newPost.querySelector('#delete-btn');
+deleteBtn.addEventListener('click', (e) => {
+  newPost.remove();
+});
+
+//Edit post
+let editBtn = newPost.querySelector('#edit-btn');
+let saveBtn = newPost.querySelector('#save-btn');
+
+editBtn.addEventListener('click', (e) => {
+  postText.outerHTML = `<input type="text" class="post" id="textarea" value="${postText.value}">`;
+  editBtn.style.display = 'none';
+  saveBtn.style.display = 'inline-block';
+});
+
+saveBtn.addEventListener('click', (e) => {
+  let inputField = newPost.querySelector('input');
+  postText.outerHTML = `<textarea class="post" id="textarea">${inputField.value}</textarea>`;
+  saveBtn.style.display = 'none';
+  editBtn.style.display = 'inline-block';
+});
+
+//*Code doesnt work below line
+////////////
+// editBtn.addEventListener('click', () => {
+//   if (editBtn.textContent === 'Edit') {
+//     newPost.removeAttribute('readonly');
+//     editBtn.textContent = 'Save';
+//   } else {
+//     newPost.setAttribute('readonly', true);
+//     editBtn.textContent = 'Edit';
+//   }
+// });
+
+// deleteBtn.addEventListener('click', (e) => {
+//   let postContainer = deleteBtn.appendDiv;
+//   postContainer.remove();
+// });
+
+// // function deletePost(
+// deleteBtn.innerHTML = 'Delete';
+// deleteBtn.onclick = function () {
+//   deletePost(newPost);
+// };
+// newPost.appendChild(deleteBtn);
+// function deletePost(newPost) {
+//   newPost.remove();
+// }
+// //Create edit button
+// editBtn = document.createElement('button');
+// editBtn.innerHTML = 'Edit';
+// editBtn.onclick = function () {
+//   editPost(newPost);
+// };
+// newPost.appendChild(editBtn);
+// function editPost(newPost) {
+//   postText.value = newPost.innerText;
+//   newPost.innerHTML = '';
+//   newPost.appendChild(postText);
+//   const saveBtn = document.createElement('button');
+//   saveBtn.innerHTML = 'Save';
+//   saveBtn.onclick = function () {
+//     savePost(newPost, postText.value);
+//   };
+//   newPost.appendChild(saveBtn);
+// }
+
+// //Save button
+// function savePost(newPost, updatedPost) {
+//   newPost.textContent = updatedPost;
+//   deleteBtn.innerHTML = 'Delete';
+//   deleteBtn.onclick = function () {
+//     deletePost(newPost);
+//   };
+//   newPost.appendChild(deleteBtn);
+// }
